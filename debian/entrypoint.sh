44b0c4289151b67c4fe62c3803a5d93e4ed847c4
ad <62818208+0adri3n@users.noreply.github.com>
2025-02-10 11:00:48 +0100

    edit vnc start

--- debian/entrypoint.sh
+++ debian/entrypoint.sh
@@ -7,13 +7,25 @@ echo "                  GitHub: https://github.com/0adri3n/linuxEverywh3re  "
 echo "######################################################################"
 echo ""
 
+# Stop any existing X server
+killall Xvfb 2>/dev/null
+
 # Start SSH service
 service ssh start
 
-# Start VNC server with XFCE
-Xvfb :1 -screen 0 1024x768x16 &
+# Start virtual display
+Xvfb :1 -screen 0 1024x768x16 +extension RANDR &
+sleep 2
+
+# Set DISPLAY and XAUTHORITY
 export DISPLAY=:1
-x11vnc -display :1 -rfbauth /root/.vnc/passwd -forever &
+export XAUTHORITY=/root/.Xauthority
+xauth generate :1 . trusted
+
+# Start VNC server
+x11vnc -display WAIT:1 -auth /root/.Xauthority -rfbauth /root/.vnc/passwd -forever &
+
+# Start XFCE
 startxfce4 &
 
 # Keep the container running
